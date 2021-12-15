b259288e56824907939dc9177b8206cb0a7d5ff6
A E <aelmollaz@gmail.com>
2021-12-15 04:09:26 +0000

    Implement character counter logic and styling

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,20 @@
+$(document).ready(function () {
+  $('#tweet-text').on('input', function (event) {
+    const value = $(this).val();
+    const counter = $(this).siblings().find('output');
+    let count = 140 - value.length;
+    
+    if (count > 140) {
+      count = 140 - count;
+    }
+    if (count < 0) {
+      $(counter).css('color', 'red')
+    }
+    if (count >= 0) {
+      $(counter).css('color', '#545149')
+    }
+
+    $(counter).text(count);
+
+  })
+});
